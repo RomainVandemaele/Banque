1b3d1e1cbc21a6ad65c8559eaff20012ae4ff6e9
rvdemael <romain.v@live.be>
2022-06-05 15:46:23 +0200

    Corection numero format dans main now numero!=null

--- src/main/java/be/bf/banque/Main.java
+++ src/main/java/be/bf/banque/Main.java
@@ -21,15 +21,22 @@ public class Main {
         CompteCourant compteC2 = new CompteCourant("BE43 0689 9999 9502", titulaire, 100, 20);
 
         Titulaire titulaire2 = new Titulaire("Bandemaele", "Romain", LocalDate.of(1993, 10, 9));
-        CompteEpargne compteE = new CompteEpargne("BE 1234 1234 1234",titulaire2,1000);
-        CompteEpargne compteE2 = new CompteEpargne("BE 1234 1234 1235",titulaire2,100);
+        CompteEpargne compteE = new CompteEpargne("BE43 1234 9999 9502",titulaire2,100);
+        CompteEpargne compteE2 = new CompteEpargne("BE12 3456 3456 3456",titulaire2,100);
         Banque b = new Banque("Picsou");
-        //b.add(compteC).add(compteE).add(compteE2).add(compteC2);
+        b.add(compteE2).add(compteE).add(compteC2).add(compteC);
+        b.addInterest();
+
+        System.out.println(b.assetCalculator(titulaire));
+        System.out.println(b.assetCalculator(titulaire2));
+
+        System.out.println(compteE);
+        System.out.println(compteE2);
 
 
     }
 
-    public static void interface() {
+    public static void interaction() {
 
     }
 
@@ -45,8 +52,6 @@ public class Main {
         System.out.println("Merci pour vos infos, nous vous avons créer un profil chez nous.");
         return new Titulaire(nom,prenom,birthday);
     }
-
-
 
 
 }
